59454768e75f1ca2bfed8ce5e86d5ab54d32b799
Gina Graci <ginagraci92@gmail.com>
2019-10-16 01:38:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+  i = 0 
+  
+  while i < collection.length
+  yield(collection[i])
+  i = i + 1
+ end
+ 
+ collection
+else 
+  puts "Hey! No block was given!"
+ end
+end
+
+collection = ['arel', 'jon', 'logan', 'spencer']
+my_each(collection) do |i|
+  puts i
 end
